30e3bc80b8bdf20c3b0623a7da6facfe8d07fc01
Iskander Rakhman <iskander-r@lambdastudents.com>
2020-10-09 16:04:48 -0700

    added apostrophees into the DATABASE_URL

--- twitoff2/app.py
+++ twitoff2/app.py
@@ -7,7 +7,7 @@ from .predict import predict_user
 def create_app():
     '''Create and configure an instance of our Flask application'''
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = getenv(DATABASE_URL)
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
 
